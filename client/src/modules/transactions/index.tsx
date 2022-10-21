c52b5fe53a514174db916fab1c42d2169e2b999b
andr11 <verligor01@gmail.com>
2022-10-21 15:24:45 +0300

    fix-issues

--- client/src/modules/transactions/index.tsx
+++ client/src/modules/transactions/index.tsx
@@ -12,12 +12,14 @@ import { Transaction } from "../../interfaces";
 import { getTransactions } from "../../services/api";
 import { Paginator } from "../../components/paginator";
 import { AxiosResponse } from "axios";
+import { Search } from "../../components/table/search";
 
 export const TransactionsPage: React.FC = () => {
   const [searchValue, setSearchValue] = useState("");
   const [currSearchParam, setCurrSearchParam] = useState("address");
 
   const [currPage, setCurrPage] = useState(1);
+  const [totalages, setTotalPages] = useState(1);
 
   const {
     data: queryData,
@@ -34,39 +36,41 @@ export const TransactionsPage: React.FC = () => {
       [currSearchParam]: searchValue ?? undefined,
       pageParam: currPage,
     });
+  }, {
+    onSuccess: ({ data }) => setTotalPages(data.totalPages)
   });
   return (
     <div className="transactions-container">
       <Header />
       {error ? (
         <p>Error while uploading transactions</p>
-      ) : isLoading ? (
-        <p>Loading...</p>
       ) : (
-        queryData && (
-          <>
+        <>
+          <Search
+            onSearchClick={() =>
+              refetch({
+                [currSearchParam]: searchValue ?? undefined,
+              })
+            }
+            currSearchParam={currSearchParam}
+            setCurrSearchParam={setCurrSearchParam}
+            searchValue={searchValue}
+            setSearchValue={setSearchValue}
+          />
+          {queryData ? (isLoading ? <p>Loading...</p> : (
             <TableBlock
-              onSearchClick={() =>
-                refetch({
-                  [currSearchParam]: searchValue ?? undefined,
-                })
-              }
-              currSearchParam={currSearchParam}
-              setCurrSearchParam={setCurrSearchParam}
-              searchValue={searchValue}
-              setSearchValue={setSearchValue}
               transactions={queryData.data.transactions}
             />
-            <Paginator
-              className="centered"
-              onClick={(pageNum) => setCurrPage(pageNum)}
-              currentPage={currPage}
-              totalPages={queryData.data.totalPages}
-            />
-          </>
-        )
+          )) : <div className="table-placeholder" />}
+          <Paginator
+            className="centered"
+            onClick={(pageNum) => setCurrPage(pageNum)}
+            currentPage={currPage}
+            totalPages={totalages}
+          />
+        </>
       )}
-      <Footer />
+      < Footer />
     </div>
   );
 };
