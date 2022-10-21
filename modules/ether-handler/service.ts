e4972ea00fe4088f048ce3a085101d955a574a76
andr11 <verligor01@gmail.com>
2022-10-21 13:00:20 +0300

    fixed-filter

--- modules/ether-handler/service.ts
+++ modules/ether-handler/service.ts
@@ -1,6 +1,6 @@
-import type QueryString from 'qs';
-import { Transaction } from '../../db/models/Transaction';
-import { createFilter } from '../../utils/filter';
+import { FilterQuery } from 'mongoose';
+import { dbTransaction, Transaction } from '../../db/models/Transaction';
+import { createEtherFilter } from '../../utils/filter';
 
 export type EthParams = {
   address?: string;
@@ -12,7 +12,9 @@ export type EthParams = {
 
 export const getEthTransactions = async (query: EthParams) => {
   const { perPage = 14, pageParam } = query;
-  const filter = createFilter(query);
+  const filter: FilterQuery<dbTransaction> = createEtherFilter(query);
+
+
   const transactions = await Transaction.find(filter)
     .skip(+perPage * (+pageParam - 1))
     .limit(+perPage);
